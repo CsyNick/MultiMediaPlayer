d3dd161e60ed6d5e80119b1d7c034c6ab0e961f9
nick_chung <nick_chung@htc.com>
2016-07-26 11:09:28 +0800

    add annotations

--- app/src/main/java/com/htc/nick/multimediaplayer/MainActivity.java
+++ app/src/main/java/com/htc/nick/multimediaplayer/MainActivity.java
@@ -3,11 +3,15 @@ package com.htc.nick.multimediaplayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.androidannotations.annotations.sharedpreferences.Pref;
+
 public class MainActivity extends AppCompatActivity {
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
