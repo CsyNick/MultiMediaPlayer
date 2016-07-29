aaf2014878b6763ee6faa8f1372526aa16671eab
chsy0208@gmail.com <chsy0208@gmail.com>
2016-07-29 11:20:49 +0800

    add new Page

---
+++ app/src/main/java/com/htc/nick/Page/MusicPlayer/MusicPlayerPresenter.java
@@ -0,0 +1,7 @@
+package com.htc.nick.Page.MusicPlayer;
+
+/**
+ * Created by nick on 7/29/16.
+ */
+public class MusicPlayerPresenter {
+}
---
+++ app/src/main/java/com/htc/nick/Page/MusicPlayer/MusicPlayerView.java
@@ -0,0 +1,8 @@
+package com.htc.nick.Page.MusicPlayer;
+
+/**
+ * Created by nick on 7/29/16.
+ */
+public class MusicPlayerView {
+
+}
